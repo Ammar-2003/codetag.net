94d5ea3f3d588ade9a43401851795f504c0aeb7a
Muhammad Ammar <ch.ammar.tech@gmail.com>
2025-06-08 12:05:02 +0500

    allowed all hosts for now

--- core/settings.py
+++ core/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1' , 'codetag.net', 'core.onrender.com']
-
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 PRE_APPS = [
