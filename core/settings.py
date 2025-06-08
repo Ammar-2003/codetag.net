52bc423f82d17f442221adbe735d6210a3ab4ba3
Muhammad Ammar <ch.ammar.tech@gmail.com>
2025-06-08 13:38:26 +0500

    Nothing Serious

--- core/settings.py
+++ core/settings.py
@@ -12,10 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from decouple import config
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-# Load environment variables from .env file
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
